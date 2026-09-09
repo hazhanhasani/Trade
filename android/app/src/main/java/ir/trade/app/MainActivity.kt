3093c31015302deee25864efc6074ad207c4dfdb
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-09 23:53:21 +0330

    feat: enable polished edge-to-edge Android UI

--- android/app/src/main/java/ir/trade/app/MainActivity.kt
+++ android/app/src/main/java/ir/trade/app/MainActivity.kt
@@ -3,11 +3,13 @@ package ir.trade.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import ir.trade.app.ui.TradeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { TradeApp() }
     }
 }
