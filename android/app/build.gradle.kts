34559a2979f6159b722761f24053b401b3cf1e6d
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-10 00:11:33 +0330

    chore: bump Android app to 0.5.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ir.trade.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.4.0"
+        versionCode = 3
+        versionName = "0.5.0"
     }
 
     if (hasPermanentSigning) {
