b893b58c0d726050151267b5411a2ddc0d9e6a65
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-09 23:11:44 +0330

    fix: use AGP 9 built-in Kotlin DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -13,15 +12,17 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.3.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
-    buildFeatures { compose = true }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
