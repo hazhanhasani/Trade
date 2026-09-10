a2e282f4c764370c6d1cd781e04eaa57ac399009
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-11 01:24:40 +0330

    Require adaptive edge calibration on Android

--- android/app/src/main/java/ir/trade/app/data/ReleaseContract.kt
+++ android/app/src/main/java/ir/trade/app/data/ReleaseContract.kt
@@ -16,6 +16,7 @@ object ReleaseContract {
         "trading.execution_quality_v2",
         "trading.multi_strategy_regime_router_v1",
         "trading.strategy_learning_v2",
+        "trading.adaptive_edge_calibration_v1",
         "trading.global_portfolio_exposure_v1",
         "trading.portfolio_rotation_v1",
         "trading.portfolio_rotation_monitor_v1",
