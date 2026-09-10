c555815ad682efc596db3c10fb4d2bbe5ba7fe47
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-10 14:24:08 +0330

    Bump Android app to 1.3.1 for new trading release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ir.trade.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.3.0"
+        versionCode = 11
+        versionName = "1.3.1"
     }
 
     if (hasPermanentSigning) {
