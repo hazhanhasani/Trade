7843f4234373ee4a3b216755da10ab4c4663fce4
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-11 03:30:28 +0330

    Test Toman normalization for wallets order books and orders

--- backend/tests/nobitex_display_money_v2_test.php
+++ backend/tests/nobitex_display_money_v2_test.php
@@ -26,4 +26,25 @@ ok(closeTo((float)$btc['raw_rialValue'],49477703.63),'raw Rial valuation must be
 ok(closeTo((float)$btc['display_value_toman'],4947770.363),'explicit display value must match Nobitex UI unit');
 ok(($out['_trade_display']['rls_per_toman']??0)===10.0,'unit metadata must be explicit');
 
+$books=NobitexDisplayMoney::orderBooksResponse(['status'=>'ok','orderbooks'=>[
+    'BTCIRT'=>['bids'=>[['494777030','0.1']], 'asks'=>[['494778000','0.2']], 'lastTradePrice'=>'494777500'],
+    'BTCUSDT'=>['bids'=>[['65000.25','0.1']], 'asks'=>[['65001.25','0.2']]],
+]]);
+ok(closeTo((float)$books['orderbooks']['BTCIRT']['bids'][0][0],49477703.0),'IRT orderbook bid price must be Toman');
+ok(closeTo((float)$books['orderbooks']['BTCIRT']['asks'][0][0],49477800.0),'IRT orderbook ask price must be Toman');
+ok(closeTo((float)$books['orderbooks']['BTCIRT']['lastTradePrice'],49477750.0),'IRT last trade price must be Toman');
+ok(closeTo((float)$books['orderbooks']['BTCUSDT']['bids'][0][0],65000.25),'USDT orderbook price must stay unchanged');
+
+$orders=NobitexDisplayMoney::ordersResponse(['status'=>'ok','orders'=>[
+    ['id'=>1,'srcCurrency'=>'btc','dstCurrency'=>'rls','price'=>'494777030','averagePrice'=>'494777000','totalOrderPrice'=>'49477703','amount'=>'0.1'],
+    ['id'=>2,'srcCurrency'=>'btc','dstCurrency'=>'usdt','price'=>'65000.25','amount'=>'0.1'],
+]]);
+$irtOrder=$orders['orders'][0];$usdtOrder=$orders['orders'][1];
+ok(($irtOrder['dstCurrency']??'')==='IRT','RLS destination currency must present as IRT');
+ok(($irtOrder['raw_dstCurrency']??'')==='rls','raw order destination currency must be retained');
+ok(closeTo((float)$irtOrder['price'],49477703.0),'IRT order price must be Toman');
+ok(closeTo((float)$irtOrder['averagePrice'],49477700.0),'IRT average order price must be Toman');
+ok(closeTo((float)$irtOrder['raw_price'],494777030.0),'raw order price must remain auditable');
+ok(closeTo((float)$usdtOrder['price'],65000.25),'USDT order price must stay unchanged');
+
 echo "nobitex_display_money_v2_test: OK\n";
